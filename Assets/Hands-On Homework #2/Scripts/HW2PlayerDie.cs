c8d5d8e56cd4d9dde3a782ff76630dae17f55b95
MustardXing <Omarw1621@gmail.com>
2026-03-21 12:18:18 -0400

    HW2 (DABE CHAKRABORTY)
    Done

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerDie.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class HW2PlayerDie : MonoBehaviour
 {
+    public GameObject endPane;
+    private string Enemy = "Enemy"; 
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.tag == Enemy)
+        {
+            endPane.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
 }
